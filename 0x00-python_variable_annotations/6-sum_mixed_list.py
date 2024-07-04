1d6b1cd9f5cf4a5d1fc5e0254760ddb2c4bfaf19
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-04 15:31:49 +0300

    function that takes a list of integers and floats using Union and returns float

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Type-annotated function sum_mixed_list which takes a list mxd_lst of integers
+and floats and returns their sum as a float."""
+
+from typing import List, Union
+
+numbers = Union[int, float]
+def sum_mixed_list(mxd_lst: List[numbers]) -> float:
+    """Return the sum of a list of integers and floats as a float."""
+    return sum(mxd_lst)
