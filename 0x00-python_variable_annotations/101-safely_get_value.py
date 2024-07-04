2fd782bffb0058cda4527189e55ea573c6fac19c
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-04 16:29:21 +0300

    use of Mapping for dict, Any, Union and TypeVar for creating generic types

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Function Annotation using TypeVar"""
+
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """Function that returns the value of a key safely"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
