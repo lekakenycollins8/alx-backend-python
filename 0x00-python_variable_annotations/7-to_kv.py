75a0fb881517cfdceae10cb81ab16490c2e4cef6
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-04 15:37:49 +0300

    function that takes a string and int or float then returns a tuple with string and float

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Type-annotated function to_kv that takes a string k and an int OR float v as
+arguments and returns a tuple containing k and the square of v. The float v
+should be annotated as a Union."""
+
+from typing import Tuple, Union
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple containing k and the square of v."""
+    return (k, v * v)
