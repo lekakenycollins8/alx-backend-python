2ec0d905dd2fabccbdf992ac71f896426ec8f070
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-08 17:12:21 +0300

    added type annotations

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,25 +3,26 @@
 
 
 import asyncio
+from typing import List
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random 
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """spawns wait random n times with specified delay
     and returns list of delays"""
-    delays = []
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays: List[float] = []
+    tasks: List[asyncio.Task] = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
 
-    results = await asyncio.gather(*tasks)
+    results: List[float] = await asyncio.gather(*tasks)
     for result in results:
         delays.append(result)
 
     # insertion sort to sort the delays
     for i in range(1, len(delays)):
-        current = delays[i]
-        prev = i - 1
+        current: float = delays[i]
+        prev: int = i - 1
         while prev >= 0 and current < delays[prev]:
             delays[prev + 1] = delays[prev]
             prev -= 1
