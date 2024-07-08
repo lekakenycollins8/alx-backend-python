7a71329219616bf84e6455829f2e8b777fba29e9
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-08 18:20:53 +0300

    measure time taken for an async function to run

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,15 +5,15 @@
 import asyncio
 from typing import List
 
-
-wait_random = __import__('0-basic_async_syntax').wait_random 
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """spawns wait random n times with specified delay
     and returns list of delays"""
     delays: List[float] = []
-    tasks: List[asyncio.Task] = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    tasks: List[asyncio.Task] = [asyncio.create_task(
+        wait_random(max_delay)) for _ in range(n)]
 
     results: List[float] = await asyncio.gather(*tasks)
     for result in results:
