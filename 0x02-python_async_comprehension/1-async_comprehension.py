d555ae6c6ac368c14be176a04df562a337fdc18e
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-09 14:06:06 +0300

    correction

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,10 @@
 """Async Comprehension"""
 
 
-from typing import
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """collect 10 random numbers using an async comprehensing"""
     return [i async for i in async_generator()]
